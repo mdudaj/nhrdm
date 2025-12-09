c3cf0457504052592079d5df018d79a5ff9aeb09
John Mduda <john.mduda@nimr.or.tz>
2025-12-09 06:59:13 +0300

    Initial commit

---
+++ static/js/contributors-carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".contributors-logos").slick({
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+  });
+});
